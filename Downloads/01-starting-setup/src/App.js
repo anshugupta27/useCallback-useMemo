cd0d5389c8666608d033748a3e3a84f8ee21fa24
Anshu Gupta <rush2anshugupta@gmail.com>
2022-12-15 13:00:19 +0530

    memo

--- Downloads/01-starting-setup/src/App.js
+++ Downloads/01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useState,useMemo} from 'react';
 import DemoOutput from './components/UI/Button/DemoOutput';
 import Button from './components/UI/Button/Button'
 import './App.css';
@@ -6,24 +6,18 @@ import './App.css';
 function App() {
   console.log("****************")
   console.log("inside app.js")
-  const[allowToggle, setAllowToggle] = useState(false);
-  const[paragraph, setParagraph] = useState(false);
+  const[listTitle, setListTitle] = useState("My List");
 
-  const allowTogglerHandler = useCallback(() => {
-    setAllowToggle(true)
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New title")
   },[])
-  const togglerParagraph = useCallback (() => {
-   if(allowToggle) {
-    setParagraph((prevPara) => !prevPara)
-   }
-  }, [allowToggle]);
 
+  const list = useMemo(() => {return [125,3,1,10,9]} , [])
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={paragraph}/>
-      <Button onClick={allowTogglerHandler}> Allow Toggle </Button>
-      <Button onClick={togglerParagraph}> see paragraph </Button>
+      <DemoOutput title={listTitle} items={list}/>
+      <Button onClick={changeTitleHandler}> change title</Button>
     </div>
   );
 }
--- Downloads/01-starting-setup/src/components/UI/Button/DemoOutput.js
+++ Downloads/01-starting-setup/src/components/UI/Button/DemoOutput.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React,{useMemo} from "react";
 
 const DemoOutput = (props) => {
-    console.log("inside demo applicatin")
+  console.log("inside demo applicatin");
+  const {items} = props.items
+  const sortedList = useMemo(() => {
+    console.log(" sorting array", items)
+    return items.sort((a, b) => a - b);
+  }, [items]);
   return (
     <div>
-      <p>{props.show ? "this is a new Demo component" : ""}</p>
+      <h1>{props.title}</h1>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
