7d85cc980bc32e12cabaf551154af0c3e7e54015
Anshu Gupta <rush2anshugupta@gmail.com>
2022-12-23 10:46:17 +0530

    redux-toolkit

--- Downloads/01-starting-project 10/src/components/Header.js
+++ Downloads/01-starting-project 10/src/components/Header.js
@@ -1,10 +1,19 @@
 import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const isAuth = useSelector(state => state.auth.isAuthenticated) //component need the value to update the UI on every state change
+  const dispatch = useDispatch(); // component also need dispatch function to pass any state change
+
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+     { isAuth && <nav>
         <ul>
           <li>
             <a href='/'>My Products</a>
@@ -13,10 +22,10 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
-      </nav>
+      </nav>}
     </header>
   );
 };
